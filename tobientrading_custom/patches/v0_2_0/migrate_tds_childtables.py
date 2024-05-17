630e151892073d9a68ae1da75035579baf84bccf
lasalesi <lars.mueller@libracore.com>
2024-05-17 23:08:33 +0200

    field order save child table replication

--- tobientrading_custom/patches/v0_2_0/migrate_tds_childtables.py
+++ tobientrading_custom/patches/v0_2_0/migrate_tds_childtables.py
@@ -3,25 +3,29 @@
 import frappe
 from frappe import _
 
+table_map = {
+    "Origins": "Technical Data Sheet Origin",
+    "TDS Certificate Table": "Technical Data Sheet Certificate",
+    "MSDS Table": "Technical Data Sheet MSDS",
+    "Customs Tariff Table": "Technical Data Sheet Customs Tariff",
+    "Sensoric Parameter Table": "Technical Data Sheet Sensoric Parameter",
+    "Physical and Chemical Parameter Table": "Technical Data Sheet Physical and Chemical Parameter",
+    "Nutritional Information Table": "Technical Data Sheet Nutritional Information",
+    "Microbiologiacal Propertie Table": "Technical Data Sheet Microbiologiacal Properties",
+    "Allergene Table": "Technical Data Sheet Allergene",
+    "Packaging and Storage Table": "Technical Data Sheet Packaging and Storage",
+    "Packaging and Storage Table II": "Technical Data Sheet Additional Packaging and Storage",
+    "Dietary Information Table": "Technical Data Sheet Dietary Information",
+    "Heavy Metal Table": "Technical Data Sheet Heavy Metal",
+    "Foreign Body Table": "Technical Data Sheet Foreign Body",
+    "Additional Information Table": "Technical Data Sheet Additional Information"
+}
+    
 def execute():
     # make sure new structures are loaded
     print("Load doctypes...")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Origin")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Certificate")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet MSDS")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Customs Tariff")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Sensoric Parameter")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Physical and Chemical Parameter")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Nutritional Information")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Microbiologiacal Properties")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Allergene")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Packaging and Storage")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Additional Packaging and Storage")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Dietary Information")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Heavy Metal")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Foreign Body")
-    frappe.reload_doc("tobientrading_custom", "doctype", "Technical Data Sheet Additional Information")
+    for source, destination in table_map.items():
+        load_structure(destination)
     
     # clean up deprecated columns (these were removed but are still in the DB)
     drop_deprecated_column("Allergene Table", "allergene")
@@ -39,33 +43,34 @@ def execute():
     
     # copy child tables
     print("Copy child tables...")
-    table_map = {
-        "Origins": "Technical Data Sheet Origin",
-        "TDS Certificate Table": "Technical Data Sheet Certificate",
-        "MSDS Table": "Technical Data Sheet MSDS",
-        "Customs Tariff Table": "Technical Data Sheet Customs Tariff",
-        "Sensoric Parameter Table": "Technical Data Sheet Sensoric Parameter",
-        "Physical and Chemical Parameter Table": "Technical Data Sheet Physical and Chemical Parameter",
-        "Nutritional Information Table": "Technical Data Sheet Nutritional Information",
-        "Microbiologiacal Propertie Table": "Technical Data Sheet Microbiologiacal Properties",
-        "Allergene Table": "Technical Data Sheet Allergene",
-        "Packaging and Storage Table": "Technical Data Sheet Packaging and Storage",
-        "Packaging and Storage Table II": "Technical Data Sheet Additional Packaging and Storage",
-        "Dietary Information Table": "Technical Data Sheet Dietary Information",
-        "Heavy Metal Table": "Technical Data Sheet Heavy Metal",
-        "Foreign Body Table": "Technical Data Sheet Foreign Body",
-        "Additional Information Table": "Technical Data Sheet Additional Information"
-    }
     for source, destination in table_map.items():
         print("...{0} to {1}".format(source, destination))
         copy_table(source, destination)
     
     print("Moving TDS child tables into app done. 🚀🚀🚀")
 
+def load_structure(table):   
+    try:
+        frappe.reload_doc("tobientrading_custom", "doctype", table)
+    except Exception as err:
+        print("{0}".format(err))
+    return
+    
 def copy_table(source, destination):
     try:
-        frappe.db.sql("""INSERT INTO `tab{destination}` 
-                     SELECT * FROM `tab{source}`;""".format(source=source, destination=destination))
+        destination_fields = []
+        for f in frappe.db.sql("""DESCRIBE `tab{destination}`;""".format(destination=destination), as_dict=True):
+            destination_fields.append(f.get('Field'))
+            
+        frappe.db.sql("""
+            INSERT INTO `tab{destination}` 
+            ({fields})
+            SELECT {fields} FROM `tab{source}`;""".format(
+                source=source, 
+                destination=destination,
+                fields="`" + "`, `".join(destination_fields) + "`"
+            )
+        )
         frappe.db.commit()
     except Exception as err:
         print("{0}".format(err))
