0c1acf584f21829afc3dc3d84e5cbdd2654c90ec
zwieble <anja.friedrich@libracore.com>
2024-09-19 14:51:52 +0200

    dashboard COA

--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, vanessa bualat and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CertificateofAnalysis(Document):
---
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis_dashboard.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2024, libracore AG and contributors
+# For license information, please see license.txt
+
+
+
+def get_data():
+    return {
+        'fieldname': 'item',
+        'transactions': [
+            {
+                'label': 'Certificate of Analysis Results',
+                'items': ['Certificate of Analysis Result']
+            }
+        ]
+    }
