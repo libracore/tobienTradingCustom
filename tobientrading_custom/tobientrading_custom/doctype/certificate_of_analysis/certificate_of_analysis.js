baedff4502678f882b718a792892e56c6c7f0419
zwieble <anja.friedrich@libracore.com>
2024-09-19 10:58:29 +0200

    add buttons that do nothing atm

--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.js
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Certificate of Analysis', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__("GBA PDF"), function(){
+			import_from_pdf("GBA");
+		}, __("Import from"));
+		frm.add_custom_button(__("GBA Excel"), function(){
+			import_from_excel("GBA");
+		}, __("Import from"));
+	}
 });
+
+function import_from_pdf(type){
+	console.log("importing data");
+}
+
+function import_from_excel(type){
+	console.log("importing data");
+}
