c47851c300703998ffe083a1934b634bc5847f24
zwieble <anja.friedrich@libracore.com>
2024-11-13 11:48:08 +0100

    more cases

--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
@@ -32,63 +32,74 @@ def create_coa_from_excel_data(data):
     data = data[1:]
     
     for row in data:
-        begin_of_analysis = datetime.strptime(row[1], '%d.%m.%Y').strftime('%Y-%m-%d')
-        end_of_analysis = datetime.strptime(row[2], '%d.%m.%Y').strftime('%Y-%m-%d')
-        
-        coa = frappe.db.exists("Certificate of Analysis", {"certificate_of_analysis": row[0]})
-        if coa:
-            coa = frappe.get_doc("Certificate of Analysis", coa)
-        else:
-            coa = frappe.new_doc("Certificate of Analysis")
-            laboratory = frappe.get_doc("Supplier", "SUP-00096").name
-            if row[5]=="":
-                if len(row[4].split("\r\n")) == 3:
-                    item_code = row[4].split("\r\n")[2]
-                elif len(row[4].split("\r\n")) == 1:
-                    frappe.throw("Item code is missing for row: {}".format(row))
+        try:
+            begin_of_analysis = datetime.strptime(row[1], '%d.%m.%Y').strftime('%Y-%m-%d')
+            end_of_analysis = datetime.strptime(row[2], '%d.%m.%Y').strftime('%Y-%m-%d')
+            
+            #create COA if not exists
+            coa = frappe.db.exists("Certificate of Analysis", {"certificate_of_analysis": row[0]})
+            if coa:
+                coa = frappe.get_doc("Certificate of Analysis", coa)
+            else:
+                coa = frappe.new_doc("Certificate of Analysis")
+                laboratory = frappe.get_doc("Supplier", "SUP-00096").name
+                if row[5]=="":
+                    parts = row[4].split("\r\n")
+                    if len(parts) == 1:
+                        frappe.log_error("Item code not found in row: {row}".format(row=row), "COA Import")
+                        continue
+                    if parts[0].strip().startswith("A"):
+                        item_code = parts[0].split[0].strip()
+                    elif parts[1].strip().startswith("A"):
+                        item_code = parts[1].split[0].strip()
+                    elif len(parts) == 3 and parts[2].strip().startswith("A"):
+                        item_code = parts[2].split[0].strip()
+                    else:
+                        item_code = row[3].split()[0].strip()
                 else:
-                    item_code = row[4].split("\r\n")[1]
-                lot_tt = row[4].split("\r\n")[0]
-            else:
-                item_code = row[4]
-                lot_tt = row[5]
-            item = frappe.get_doc("Item", item_code)
-            
-            coa.update({
-                "certificate_of_analysis": row[0],
-                "item": item_code,
-                "item_name": item.item_name,
-                "item_group": item.item_group,
-                "date_coa": end_of_analysis,
-                "begin_of_analysis": begin_of_analysis,
-                "end_of_analysis": end_of_analysis,
-                "laboratory": "SUP-00096"
-            })
-        
-        coa.save()
+                    item_code = row[4].split[0].strip()
 
-        # Create COA result if parameter, result and unit are available
-        if row[7] and row[8] and row[9]:
-            parameter_name = row[7].strip()
-            parameter = frappe.db.exists("Measurement Parameter", parameter_name)
-            if parameter:
-                parameter = frappe.get_doc("Measurement Parameter", parameter_name)
-            else:
-                parameter = frappe.new_doc("Measurement Parameter")
-                parameter.parameter = parameter_name
-                parameter.save()
+                item = frappe.get_doc("Item", item_code)
+                
+                coa.update({
+                    "certificate_of_analysis": row[0],
+                    "item": item_code,
+                    "item_name": item.item_name,
+                    "item_group": item.item_group,
+                    "date_coa": end_of_analysis,
+                    "begin_of_analysis": begin_of_analysis,
+                    "end_of_analysis": end_of_analysis,
+                    "laboratory": "SUP-00096"
+                })
             
-            coa_result = frappe.new_doc("Certificate of Analysis Result")
-            coa_result.update({
-                "parameter": parameter_name, 
-                "coa": coa.name,
-                "coa_date": end_of_analysis,
-                "result": row[8],
-                "unit": row[9],
-                "max_level": row[10]
-            })
-            
-            coa_result.save()
             coa.save()
-    
+
+            # Create COA result if parameter, result and unit are available
+            if row[7] and row[8] and row[9]:
+                parameter_name = row[7].strip()
+                if "<" in parameter_name or ">" in parameter_name:
+                        parameter_name = parameter_name.replace("<", "").replace(">", "")
+                parameter = frappe.db.exists("Measurement Parameter", parameter_name)
+                if parameter:
+                    parameter = frappe.get_doc("Measurement Parameter", parameter_name)
+                else:
+                    parameter = frappe.new_doc("Measurement Parameter")
+                    parameter.parameter = parameter_name
+                    parameter.save()
+                
+                coa_result = frappe.new_doc("Certificate of Analysis Result")
+                coa_result.update({
+                    "parameter": parameter_name, 
+                    "coa": coa.name,
+                    "coa_date": end_of_analysis,
+                    "result": row[8],
+                    "unit": row[9],
+                    "max_level": row[10]
+                })
+                
+                coa_result.save()
+                coa.save()
+        except Exception as e:
+            frappe.log_error("Error while creating COA for row {row} from Excel data: {error}".format(row=row, error=e), "COA Import")
+            continue
     return "COA created successfully"
