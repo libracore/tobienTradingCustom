c4929937672ddb5044001eb415af2cf8e0fa899e
zwieble <anja.friedrich@libracore.com>
2024-11-25 10:32:24 +0100

    first working draft

--- tobientrading_custom/public/js/certificate_of_analysis_list.js
+++ tobientrading_custom/public/js/certificate_of_analysis_list.js
@@ -16,21 +16,29 @@ function load_script(url, callback) {
 frappe.listview_settings['Certificate of Analysis'] = {
     onload: function(listview) {
         listview.page.add_menu_item(__("Import from Excel"), function() {
-            import_from_excel();
+            import_file("excel");
+
+        });
+        listview.page.add_menu_item(__("Import from XML"), function() {
+            import_file("xml");
 
         });
     }
 }
 
-function import_from_excel(){
+function import_file(type){
 	var input = document.createElement("input");
 	input.type = 'file';
-	input.accept = '.xls,.xlsx';
+	if (type == "excel") {
+		input.accept = '.xls,.xlsx';
+	} else if (type == "xml") {
+		input.accept = '.xml';
+	}
 
 	input.addEventListener('change', function (event) {
 		var file = event.target.files[0];
 		if (file) {
-			read_file(file);
+			read_file(file, type);
 		} else {
 			frappe.msgprint("No file selected");
 		}
@@ -38,24 +46,53 @@ function import_from_excel(){
 	input.click();
 }
 
-function read_file(file){
+function read_file(file, type){
 	var reader = new FileReader();
 	reader.addEventListener('load', function (event) {
 		var contents = event.target.result;
-		extract_data(contents);
+		if (type == "excel") {
+			extract_data_from_excel(contents);
+		} else if (type == "xml") {
+			extract_data_from_xml(contents);
+		}
 	});
 	reader.onerror = function (event) {
 		frappe.msgprint("Error reading file");
 	};
-	reader.readAsBinaryString(file);
+	if (type == "excel") {
+		reader.readAsBinaryString(file);
+	} else if (type == "xml") {
+		reader.readAsText(file);
+	}
 }
 
-function extract_data(contents){
+function extract_data_from_xml(contents){
+	var parser = new DOMParser();
+	var xmlDoc = parser.parseFromString(contents, "application/xml");
+	create_coa_from_xml_data(xmlDoc);
+}
+
+function extract_data_from_excel(contents){
 	var workbook = XLSX.read(contents, {type: 'binary'});
 	var sheet_name_list = workbook.SheetNames[0];
 	var worksheet = workbook.Sheets[sheet_name_list];
-	var data = XLSX.utils.sheet_to_json(worksheet, {header: 1});
-	create_coa_from_excel_data(data);
+	var excelData = XLSX.utils.sheet_to_json(worksheet, {header: 1});
+	create_coa_from_excel_data(excelData);
+}
+
+function create_coa_from_xml_data(data){
+	var xmlString = (new XMLSerializer()).serializeToString(data);
+	frappe.call({
+		'method': "tobientrading_custom.tobientrading_custom.doctype.certificate_of_analysis.certificate_of_analysis.create_coa_from_xml_data",
+		'args': {
+			'data': xmlString
+		},
+		'callback': function(response){
+			if (response.message) {
+				frappe.msgprint(response.message);
+			}
+		}
+	})
 }
 
 function create_coa_from_excel_data(data){
--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis/certificate_of_analysis.py
@@ -5,6 +5,7 @@ import frappe
 from frappe.model.document import Document
 import json
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 class CertificateofAnalysis(Document):
     pass
@@ -36,7 +37,7 @@ def create_coa_from_excel_data(data):
             begin_of_analysis = datetime.strptime(row[1], '%d.%m.%Y').strftime('%Y-%m-%d')
             end_of_analysis = datetime.strptime(row[2], '%d.%m.%Y').strftime('%Y-%m-%d')
             
-            #create COA if not exists
+            # Create COA if not exists
             coa = frappe.db.exists("Certificate of Analysis", {"certificate_of_analysis": row[0]})
             if coa:
                 coa = frappe.get_doc("Certificate of Analysis", coa)
@@ -48,7 +49,7 @@ def create_coa_from_excel_data(data):
                     parts = row[4].split("\r\n")
                     item_code = next((split_and_strip(part) for part in parts if part.strip().startswith("A")), None)
                     if not item_code:
-                        #try to find item code in row 3
+                        # Try to find item code in row 3
                         item_code = split_and_strip(row[3])
                         item = frappe.db.exists("Item", item_code)
                         if not item:
@@ -82,11 +83,11 @@ def create_coa_from_excel_data(data):
             
             coa.save()
 
-            # Create COA result if parameter, result and unit are available
+            # Create COA result if parameter, result, and unit are available
             if row[7] and row[8] and row[9] and row[8] != "\xa0":
                 parameter_name = row[7].strip()
                 if "<" in parameter_name or ">" in parameter_name:
-                        parameter_name = parameter_name.replace("<", "").replace(">", "")
+                    parameter_name = parameter_name.replace("<", "").replace(">", "")
                 parameter = frappe.db.exists("Measurement Parameter", parameter_name)
                 if parameter:
                     parameter = frappe.get_doc("Measurement Parameter", parameter_name)
@@ -111,7 +112,76 @@ def create_coa_from_excel_data(data):
         except Exception as e:
             frappe.log_error("Error while creating COA for row {row} from Excel data: {error}".format(row=row, error=e), "COA Import")
             continue
+
     return "COA created. Check logs for potential errors."
+
+@frappe.whitelist()
+def create_coa_from_xml_data(data):
+    soup = BeautifulSoup(data, "lxml")
+    coa_name = soup.find("sample_sample_name").text
+    
+    # Create COA if not exists
+    coa = frappe.db.exists("Certificate of Analysis", {"certificate_of_analysis": coa_name})
+    if coa:
+        coa = frappe.get_doc("Certificate of Analysis", coa)
+    else:
+        coa = frappe.new_doc("Certificate of Analysis")
+
+        end_of_analysis = datetime.strptime(soup.find("Reportdate").text, '%Y-%m-%d').strftime('%Y-%m-%d')
+        item_code = soup.find("Referencenumber").text
+
+        item = frappe.get_doc("Item", item_code)
+        batch = soup.find("BatchLotnumber").text
+
+        if frappe.db.exists("Batch", batch):
+            batch_tt = batch
+        else:
+            batch_tt = ""
+            frappe.log_error("Batch {batch} not found".format(batch=batch), "COA Import: Batch not found")
+
+        coa.update({
+            "certificate_of_analysis": coa_name,
+            "item": item_code,
+            "item_name": item.item_name,
+            "item_group": item.item_group,
+            "date_coa": end_of_analysis,
+            "begin_of_analysis": end_of_analysis,
+            "end_of_analysis": end_of_analysis,
+            "laboratory": "SUP-00381",
+            "batch_tt": batch_tt
+        })
+
+    coa.save()
+
+    # Create COA result if parameter, result, and unit are available
+    for row in soup.find_all("row"):
+        if row[7] and row[8] and row[9] and row[8] != "\xa0":
+            parameter_name = row[7].strip()
+            if "<" in parameter_name or ">" in parameter_name:
+                parameter_name = parameter_name.replace("<", "").replace(">", "")
+            parameter = frappe.db.exists("Measurement Parameter", parameter_name)
+            if parameter:
+                parameter = frappe.get_doc("Measurement Parameter", parameter_name)
+            else:
+                parameter = frappe.new_doc("Measurement Parameter")
+                parameter.parameter = parameter_name
+                parameter.save()
+
+            coa_result = frappe.new_doc("Certificate of Analysis Result")
+            coa_result.update({
+                "parameter": parameter_name,
+                "test_type": parameter.test_type,
+                "coa": coa.name,
+                "coa_date": end_of_analysis,
+                "result": row[8],
+                "unit": row[9],
+                "max_level": row[10]
+            })
+
+            coa_result.save()
+            coa.save()
+
+    return coa_name
 
 def split_and_strip(string, idx=0):
     return string.split()[idx].strip()
