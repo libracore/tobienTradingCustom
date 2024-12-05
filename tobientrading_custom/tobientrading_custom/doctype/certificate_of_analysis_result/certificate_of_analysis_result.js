f200e0c2489f032fdadce90a95c2b730bd2d8f60
zwieble <anja.friedrich@libracore.com>
2024-12-05 10:57:08 +0100

    eo

--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis_result/certificate_of_analysis_result.js
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis_result/certificate_of_analysis_result.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Certificate of Analysis Result', {
-	// refresh: function(frm) {
-
-	// }
+	parameter: function(frm) {
+		fetch_test_type_and_subcategory(frm);
+	}
 });
+
+
+function fetch_test_type_and_subcategory(frm){
+	console.log(frm.doc.parameter);
+	frappe.call({
+		'method': "tobientrading_custom.tobientrading_custom.doctype.certificate_of_analysis_result.certificate_of_analysis_result.get_test_type_and_subcategory",
+		'args': {
+			'parameter': frm.doc.parameter
+		},
+		'callback': function(response){
+			console.log(response.message);
+			if (response.message) {
+				frm.set_value('test_type', response.message.test_type);
+				frm.set_value('test_type_subcategory', response.message.subcategory);
+			}
+		}
+	})
+}
--- tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis_result/certificate_of_analysis_result.py
+++ tobientrading_custom/tobientrading_custom/doctype/certificate_of_analysis_result/certificate_of_analysis_result.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2024, libracore AG and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CertificateofAnalysisResult(Document):
 	pass
+
+@frappe.whitelist()
+def get_test_type_and_subcategory(parameter):
+	query = """
+		SELECT `test_type`, `subcategory`
+		FROM `tabMeasurement Parameter`
+		WHERE `parameter` = '{parameter}'
+	""".format(parameter=parameter)
+
+	results = frappe.db.sql(query, as_dict=True)
+	return results
