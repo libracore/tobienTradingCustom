8e95e6f7bc64ff45adfb4c7241f56b80419ee41d
Martin Zoller <martin.zoller@libracore.com>
2026-06-19 18:11:47 +0200

    Port of double-scrolling fix (erpfinkzeit#34) to customer app

--- tobientrading_custom/hooks.py
+++ tobientrading_custom/hooks.py
@@ -15,7 +15,10 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/tobientrading_custom/css/tt.css"
+app_include_css = [
+    "/assets/tobientrading_custom/css/tt.css",
+    "tobientrading_custom.bundle.css"
+]
 app_include_js = [
     "/assets/tobientrading_custom/js/tobien_common.js"
 ]
